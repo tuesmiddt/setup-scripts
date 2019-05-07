a9009df8cdb8e3e386ae17d9fa01ecadbbe2ac9f
Shaowei Chen <dev@chenshaowei.com>
2019-05-07 03:42:05 +0000

    Add ufw script

---
+++ setup_ufw.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+
+# read -p "Set ufw rules? [y/N] " -n 1 -r
+# echo
+# if [[ ! $REPLY =~ ^[Yy]$ ]] 
+# then
+#   echo "Aborting..."
+#   [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+# fi
+if [[ "$UID" -ne "0" ]]; then
+  echo "This script must be run as root (sudo)"
+  echo "Aborting..."
+  [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+echo "Enabling logs..."
+ufw logging on
+echo "Done."
+
+echo "Rate limiting ssh connections..."
+ufw limit ssh
+echo "Done."
+
+mapfile -t app_list < <( ufw app list )
+unset app_list[0]
+
+echo "Setting rules for installed services"
+for app in "${app_list[@]}"
+do
+  echo "Service name: $app"
+  read -p "Allow? [y/N]" -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    echo "Allowing $app..."
+    ufw allow $app
+  else
+    echo "Denying $app..."
+    ufw deny $app
+  fi
+done
+echo "Done."
+
+read -p "Allow incoming HTTP connections? [y/N]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Allowing HTTP..."
+  ufw allow http
+else
+  echo "Denying HTTP..."
+  ufw deny http
+fi
+
+read -p "Allow incoming HTTPS connections? [y/N]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Allowing HTTPS..."
+  ufw allow https
+else
+  echo "Denying HTTPS..."
+  ufw deny https
+fi
+
+echo
+echo "Showing changed rules"
+ufw show added
+echo
+
+read -p "Commit added rules? [y/N]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  ufw enable
+  ufw reload
+fi
+
+echo "Current ufw rules:"
+ufw status verbose numbered
+
+read -p "Press any key to continue..." -n 1 -r
+
+echo "Done."
+[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
