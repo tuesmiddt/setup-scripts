610e65dd6594ffa0533e6f0d304811af3e7320e3
Shaowei Chen <dev@chenshaowei.com>
2019-05-07 15:10:39 +0000

    Add dotfile setup script

---
+++ setup_dotfiles_cli.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+source util.sh
+
+set_link $(realpath dotfiles/.vimrc) "$HOME/.vimrc"
---
+++ util.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Usage: link_file source_realpath target_path
+set_link () {
+  echo "Setting $2"
+  target_dir=$(dirname $2)
+  if [[ ! -d "$target_dir" ]]
+  then
+    mkdir -p $target_dir
+  fi
+
+  if [[ -L "$2" ]]
+  then
+    current_link=$(realpath $(readlink $2))
+
+    if [[ $1 = $current_link ]]
+    then
+      return
+    fi
+    echo "$2 symlink already exists."
+    echo "$2 currently linked to $current_link"
+    
+    read -p "Remove existing link? [y/N] " -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+      echo "Skipping $2"
+      return
+    fi
+    
+    rm $2
+  elif [[ -f "$2" ]]
+  then
+    echo "$2 already exists."
+    echo "Backing up to $2.bak"
+    cp $2 $2.bak
+    rm $2
+  fi
+
+  ln -s $1 $2
+}
