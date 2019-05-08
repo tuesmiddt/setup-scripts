a6e90d6a584e93aabdfbfc0d9c5d59bfba4741a0
Shaowei Chen <dev@chenshaowei.com>
2019-05-08 13:01:18 +0000

    Fix glue script

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-setup_git.sh
+./setup_git.sh
 
-setup_ufw.sh
+./setup_ufw.sh
 
-sudo setup_apt_mirrors.sh
-sudo install_packages.sh apt_install.list
+sudo ./setup_apt_mirrors.sh
+sudo ./install_packages.sh apt_install.list
 
-setup_yarn.sh
-setup_nvim.sh
+./setup_yarn.sh
+./setup_nvim.sh
 
-setup_zsh.sh
+./setup_zsh.sh
