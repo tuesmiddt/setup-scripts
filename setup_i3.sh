695e14fab3cb45f39cb6dfee8004cfaba8b9f8c9
Shaowei Chen <dev@chenshaowei.com>
2019-08-12 17:09:40 +0800

    Add compton

--- setup_i3.sh
+++ setup_i3.sh
@@ -6,3 +6,4 @@ sudo mv /usr/bin/ksplashqml /usr/bin/ksplashqml.old
 set_link $(realpath wm.sh) "$HOME/.config/plasma-workspace/env/wm.sh"
 set_link $(realpath dotfiles/i3/config) "$HOME/.config/i3/config"
 set_link $(realpath dotfiles/.i3status.conf) "$HOME/.config/.i3status.conf"
+set_link $(realpath dotfiles/compton.conf) "$HOME/.config/compton.conf"
